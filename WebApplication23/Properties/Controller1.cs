82e5c2ceffcfa63b857832de599a9fbd466e4d33
virusboxx <166071169+virusboxx@users.noreply.github.com>
2024-09-15 01:28:24 +0330

    add new file

---
+++ WebApplication23/Properties/Controller1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication23.Properties;
+
+public class Controller1 : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
